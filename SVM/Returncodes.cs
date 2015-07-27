039091e0661b8be51685451f45e96528d37649a1
voidbab <niko.halikka@gmail.com>
2015-07-27 06:07:44 +0300

    changed static to const from returncodes

--- SVM/Returncodes.cs
+++ SVM/Returncodes.cs
@@ -12,22 +12,22 @@ namespace SVM
         /// <summary>
         /// Default return code.
         /// </summary>
-        public static byte DEFAULT_RET_CODE = 0;
+        public const byte DEFAULT_RET_CODE = 0;
 
         /// <summary>
         /// Debug exception was thrown.
         /// </summary>
-        public static byte DEBUG_EXCEPTION = 1;
+        public const byte DEBUG_EXCEPTION = 1;
 
         /// <summary>
         /// Abort has been called.
         /// </summary>
-        public static byte ABORT_CALLED = 2;
+        public const byte ABORT_CALLED = 2;
 
         /// <summary>
         /// pc is in invalid state or invalid opcode was found.
         /// </summary>
-        public static byte PC_CORRUPTED_OR_NOT_OPCODE = 3;
+        public const byte PC_CORRUPTED_OR_NOT_OPCODE = 3;
         #endregion
     }
 }
